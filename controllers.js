a99e7880fa075e641ee091012b02bf221781e713
Drf4 <60240949+Drf4@users.noreply.github.com>
2020-12-12 12:08:03 -0500

    Create controllers.js

---
+++ controllers.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Gym_Equipment = mongoose.model('Gym Equipment'); // defined in model.js
+
+exports.list_gym_equipment = function(req, res) {
+  Gym_Equipment.find({}, function(err, gym_equipment) {
+    if (err)
+      res.send(err);
+    res.json(gym_equipment);
+  });
+};
+
+exports.get_gym_equipment = function(req, res) {
+  Gym_Equipment.findById(req.params.id, function(err, gym_equipment) {
+    if (err)
+      res.send(err);
+    res.json(gym_equipment);
+  });
+};
+
+exports.update_gym_equipment = function(req, res) {
+  Gym_Equipment.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function(err, gym_equipment) {
+    if (err)
+      res.send(err);
+    res.json(gym_equipment);
+  });
+};
+
+exports.delete_gym_equipment = function(req, res) {
+  Gym_Equipment.remove({
+    _id: req.params.id
+  }, function(err, gym_equipment) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Food bank successfully deleted' });
+  });
+};
+
+exports.create_gym_equipment = function(req, res) {
+  var gym_equipment = new Gym_Equipment(req.body);
+  gym_equipment.save(function(err, gym_equipment) {
+    if (err)
+      res.send(err);
+    res.json(gym_equipment);
+  });
+};
